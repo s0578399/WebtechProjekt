480f83ddc2e5c5bb97c676238eee3fdb12aa8c77
Elias Rogov <eliasrogov@gmail.com>
2021-11-10 21:48:51 +0100

    M2 REST API: Person lesen

--- src/main/java/de/htwberlin/webtech/webtech/web/PersonRestController.java
+++ src/main/java/de/htwberlin/webtech/webtech/web/PersonRestController.java
@@ -4,10 +4,7 @@ import de.htwberlin.webtech.webtech.service.PersonService;
 import de.htwberlin.webtech.webtech.web.api.Person;
 import de.htwberlin.webtech.webtech.web.api.PersonCreateRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -25,6 +22,13 @@ public class PersonRestController {
 
     @GetMapping(path = "/api/v1/persons")
     public ResponseEntity<List<Person>> kunden() {return ResponseEntity.ok(personService.findAll());}
+
+
+    @GetMapping(path = "/api/v1/persons/{id}")
+    public ResponseEntity<Person> kundenByID(@PathVariable Long id) {
+        var person = personService.findById(id);
+        return person != null? ResponseEntity.ok(person) : ResponseEntity.notFound().build();
+    }
 
 
     @PostMapping(path = "/api/v1/persons")
